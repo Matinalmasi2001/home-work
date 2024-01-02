426e27a485608f8e221308565ef902e8ffb6f077
Matinalmasi2001 <151912059+Matinalmasi2001@users.noreply.github.com>
2024-01-02 18:06:47 +0330

    Add files via upload

---
+++ hw6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+int m,n;
+cout << " enter 2 positive num: " ;
+cin >> m >> n ;
+if(m>0 and n>0){
+	int result = pow(m,n);
+cout<< result;
+}
+}
+
