e6e66b85799070a6e94e92ca8eaf26231041b00a
Matinalmasi2001 <151912059+Matinalmasi2001@users.noreply.github.com>
2024-01-02 17:54:04 +0330

    Add files via upload

---
+++ hw.cpp
@@ -0,0 +1,18 @@
+  #include <iostream>
+
+using namespace std;
+
+ main() {                           
+   float a,b,jam,taf,zarb,baghi;
+   cout<< "enter your float numbers: ";
+   cin>>a>>b;
+   
+   jam = (float) a+b;
+   taf = (float) a-b;
+   zarb = (float) a*b;
+   baghi = (float)a/b;
+   
+   cout<<"jam: "<<jam<<" tafrigh: "<<taf<<" zarb: "<<zarb<<" baghimande: "<<baghi;
+  
+}
+
